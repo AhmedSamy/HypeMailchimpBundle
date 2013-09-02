62604b2181dd0fe6583c764bd0ecc7320a27a09b
Ahmed Samy <ahmed.samy.cs@gmail.com>
2013-09-03 01:06:58 +0200

    Commit

--- Hype/MailchimpBundle/HypeMailchimpBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Hype\MailchimpBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class HypeMailchimpBundle extends Bundle
-{
-}
--- Hype/MailchimpBundle/README.md
+++
@@ -1,4 +0,0 @@
-Hype Mailchimp V2.0
-========================
-
-MailChimp API V2.0
